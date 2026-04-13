f42afabaf648579258c140c0642113774fed9805
Juan Alma <juanangelaalma@gmail.com>
2026-04-13 18:22:12 +0700

    feat: add more demo section

---
+++ app/_sections/MoreDemoSection.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import img1 from "../__assets/demo/1.webp";
+import img2 from "../__assets/demo/2.webp";
+import img3 from "../__assets/demo/3.webp";
+
+export default function MoreDemoSection() {
+    const images = [img1, img2, img3];
+
+    return (
+        <section className="bg-black p-[80px] w-full overflow-hidden">
+            <div className="flex overflow-x-auto gap-4 md:gap-6 px-4 md:px-8 sm:px-6 lg:px-32 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] snap-x snap-mandatory">
+                {images.map((src, index) => (
+                    <div
+                        key={index}
+                        className="relative flex-none w-[75vw] sm:w-[50vw] md:w-[35vw] lg:w-[450px] aspect-[9/16] rounded-2xl overflow-hidden snap-center"
+                    >
+                        <Image
+                            src={src}
+                            alt={`Demo image ${index + 1}`}
+                            fill
+                            className="object-cover"
+                        />
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
