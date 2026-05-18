9589a257d826e626f10941b9838c0fcfeddcdb55
Juan Alma <juanangelaalma@gmail.com>
2026-05-18 22:20:53 +0700

    Add showcase video tiles

--- app/_sections/MoreDemoSection.tsx
+++ app/_sections/MoreDemoSection.tsx
@@ -1,25 +1,21 @@
-import Image from "next/image";
-import img1 from "../__assets/demo/1.webp";
-import img2 from "../__assets/demo/2.webp";
-import img3 from "../__assets/demo/3.webp";
+import MoreDemoVideoTile from "./MoreDemoVideoTile";
+
+const videos = [
+    { src: "/showcase-video/eyelash", label: "Eyelash extension demo" },
+    { src: "/showcase-video/leaves", label: "Floral portrait demo" },
+    { src: "/showcase-video/car", label: "Luxury car showroom demo" },
+];
 
 export default function MoreDemoSection() {
-    const images = [img1, img2, img3];
-
     return (
         <section className="bg-black pb-[80px] pl-[16px] lg:p-[80px] w-full overflow-hidden">
             <div className="flex overflow-x-auto pr-[16px] md:pr-[0px] gap-4 md:gap-6 [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none] snap-x snap-mandatory base-container">
-                {images.map((src, index) => (
+                {videos.map((video) => (
                     <div
-                        key={index}
+                        key={video.src}
                         className="relative flex-none w-[75vw] sm:w-[50vw] md:w-[35vw] lg:w-[450px] lg:h-[800px] aspect-[9/16] rounded-2xl overflow-hidden snap-center"
                     >
-                        <Image
-                            src={src}
-                            alt={`Demo image ${index + 1}`}
-                            fill
-                            className="object-cover"
-                        />
+                        <MoreDemoVideoTile src={video.src} label={video.label} />
                     </div>
                 ))}
             </div>
